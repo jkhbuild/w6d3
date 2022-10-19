64362921c87924bca063fcf81f4e69444a54c932
jkhbuild <jkh.build@gmail.com>
2022-10-19 14:41:23 -0400

    phase 1 completed

--- firstroutesandcontrollers/config/routes.rb
+++ firstroutesandcontrollers/config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  # resources :users
+
+  get 'users/:id', to: 'users#show' #, as: 'user'
+  get 'users/', to: 'users#index' #, as: 'user'
+  get 'users/new', to: 'users#new' #, as: 'user'
+  get 'users/:id/edit', to: 'users#edit' #, as: 'user'
+  post 'users/', to: 'users#create' #, as: 'user'
+  patch 'users/:id', to: 'users#update' #, as: 'user'
+  delete 'users/:id', to: 'users#destroy' #, as: 'user'
+  put 'users/:id', to: 'users#update' #, as: 'user'
+
 end
